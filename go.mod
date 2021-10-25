c00ae18559293dea1e4f020c494e5f05fb2bcc8d
suwa <baru.web.production@gmail.com>
2021-10-25 21:40:41 +0900

    secound commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gotodo
+
+go 1.17
+
+require github.com/lib/pq v1.10.3 // indirect
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"text/template"
+
+	_ "github.com/lib/pq"
+)
+
+type Task struct {
+	Id       int
+	Title    string
+	Describe string
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/destory", destory)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func dbConn() (db *sql.DB) {
+	connStr := "user=suwayouta dbname=todo sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
+var templates = template.Must(template.ParseGlob("view/*"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	sql := "SELECT * FROM tasks ORDER BY id DESC"
+	rows, err := db.Query(sql)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		log.Fatalln(err)
+	}
+	var ts []Task
+	for rows.Next() {
+		var t Task
+		err := rows.Scan(&t.Id, &t.Title, &t.Describe)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln(err)
+		}
+		ts = append(ts, t)
+	}
+	templates.ExecuteTemplate(w, "index.html", ts)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		sql := "INSERT INTO tasks (title, describe) VALUES ($1, $2)"
+		t := r.FormValue("title")
+		d := r.FormValue("describe")
+		if t == "" {
+			t = "タイトルなし"
+		}
+		if d == "" {
+			d = "説明なし"
+		}
+		_, err := db.Exec(sql, t, d)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln(err)
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func destory(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
