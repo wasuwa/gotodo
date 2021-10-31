d696075f08b2a91c56586fea86ca73526b6fe321
suwa <baru.web.production@gmail.com>
2021-10-31 12:06:09 +0900

    ParseGlobメソッドの引数のパスを修正する

--- controllers/tasks_controller.go
+++ controllers/tasks_controller.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var templates = template.Must(template.ParseGlob("views/*"))
+var templates = template.Must(template.ParseGlob("../views/*"))
 
 func TaskIndex(w http.ResponseWriter, r *http.Request) {
 	var t models.Task
